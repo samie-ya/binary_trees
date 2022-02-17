eb686f48f537bbd8e9f1c38fe717f8a2cea36f27
Kidus Worku <kidusmik@gmail.com>
2022-02-17 08:07:51 +0000

    add 1-binary_tree_insert_left.c

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node;
+
+	if (parent == NULL)
+		return NULL;
+
+        node = malloc(sizeof(binary_tree_t));
+	if (node == NULL)
+		return NULL;
+
+	if (parent->left != NULL)
+		node->left = parent->left;
+
+	parent->left = node;
+	node->n = value;
+	return node;
+}
