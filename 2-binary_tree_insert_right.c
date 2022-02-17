d1b59b56aaca0bdfe35dbcd136bd980f401f1c96
Samie <samrasolomon40@gmail.com>
2022-02-17 09:57:54 +0000

    Modified 2-Binary

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -9,7 +9,7 @@
 */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *node;
+	binary_tree_t *node, *rt = NULL;
 
 	if (parent == NULL)
 		return (NULL);
@@ -18,10 +18,20 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (node == NULL)
 		return (NULL);
 
-	if (parent->right != NULL)
-		node->right = parent->right;
-
-	parent->right = node;
 	node->n = value;
+	node->parent = parent;
+	node->left = NULL;
+	if (parent->right != NULL)
+	{
+		rt = parent->right;
+		parent->right = node;
+		node->right = rt;
+		rt->parent = node;
+	}
+	else
+	{
+		parent->right = node;
+		node->right = NULL;
+	}
 	return (node);
 }
