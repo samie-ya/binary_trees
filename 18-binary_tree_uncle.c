35ba6c4ef1bf7a9727769b2b58cb97368b69551c
Kidus Worku <kidusmik@gmail.com>
2022-02-17 19:57:53 +0000

    add 18-binary_tree_uncle.c

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,35 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_sibling - determine the sibling of a node
+* @node: the node
+*
+* Return: pointer to a binary_tree_t
+*/
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	if (node == node->parent->left)
+		return (node->parent->right);
+	else
+		return (node->parent->left);
+}
+
+/**
+* binary_tree_uncle - determines the uncle of the node
+* @node: the node
+*
+* Return: pointer to a binary_tree_tw
+*/
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+        if (node == NULL)
+                return (NULL);
+
+        if (node->parent == NULL)
+                return (NULL);
+
+        return (binary_tree_sibling(node->parent));
+}
