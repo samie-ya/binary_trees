0b29845230177c6957530e91612503b57c32828d
Samie <samrasolomon40@gmail.com>
2022-02-17 19:54:59 +0000

    Modified 17-binary

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -10,19 +10,11 @@
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	binary_tree_t *parent = NULL;
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
 
-	parent = node->parent;
-	if (node == NULL)
-		return (NULL);
-	if (parent == NULL)
-		return (NULL);
-	if (parent->left != NULL || parent->right != NULL)
-	{
-		if (parent->left == node)
-			return (parent->right);
-		else if (parent->right == node)
-			return (parent->left);
-	}
-	return (NULL);
+	if (node == node->parent->left)
+		return (node->parent->right);
+	else
+		return (node->parent->left);
 }
